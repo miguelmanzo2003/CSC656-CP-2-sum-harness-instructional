741460b94dacd3289fccbe78e99a07e5deb07f5f
Miguel Manzo <manzo@login34.chn.perlmutter.nersc.gov>
2025-10-25 02:50:41 -0700

    ran python file, added charts

--- plot_3vars_savefig.py
+++ plot_3vars_savefig.py
@@ -19,9 +19,9 @@ Assumptions: developed and tested using Python version 3.8.8 on macOS 11.6
 import pandas as pd
 import matplotlib.pyplot as plt
 
-plot_fname = "myplot.png"
+plot_fname = "PeakMemory.png"
 
-fname = "sample_data_3vars.csv"
+fname = "PeakMemory.csv"
 df = pd.read_csv(fname, comment="#")
 print(df)
 
@@ -39,7 +39,7 @@ code3_time = df[var_names[3]].values.tolist()
 
 plt.figure()
 
-plt.title("Comparison of 3 Codes")
+plt.title("Direct Sum vs Indirect Sum vs Vector Sums")
 
 xlocs = [i for i in range(len(problem_sizes))]
 
@@ -53,7 +53,7 @@ plt.plot(code3_time, "g-^")
 #plt.yscale("log")
 
 plt.xlabel("Problem Sizes")
-plt.ylabel("runtime")
+plt.ylabel("% of Memory Bandwidth Utilized")
 
 varNames = [var_names[1], var_names[2], var_names[3]]
 plt.legend(varNames, loc="best")
